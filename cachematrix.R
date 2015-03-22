66d14a0e677dc3ad60189eac3211cd425297e998
charalak_git <kanelcha@gmail.com>
2015-03-22 15:52:09 +0100

    code works but explanation is needed

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,63 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The purpose of this function is first to create a matrix with the
+## makeCacheMatrix, which is assumed that can be inverse. Then, 
+## function cacheSolve solves the given matrix from the previous
+## function and iverse it.
 
-## Write a short comment describing this function
+## The makeCacheMatrix creates a matrix that is assumed a priori
+## to can be inverse. The argument of x or y should be given in
+## as the following example: "x <- matrix( c(1,2, 3, 4),nrow=2,ncol=2)" 
+## one can set the matrix and its inverse with seMat and setInverseMat
+## respectivly. In addition, one can get the matrix and its inverse with 
+## geMat and getInverseMat respectivly.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        
+        inv <- NULL  ## i.e. the NULL value set initially
+                     ## to the inverse matrix before calculated
+        
+        ## the setmat function is to set a matrix different than
+        ## x matrix when we want it. The y matrix is then assigned
+        ## to the x value.
+        setMat <- function(y){
+                x <<- y 
+                inv <<- NULL
+        }
+        
+        ## getmat function returns the matrix
+        getMat <- function() x
+        
+        ## setInverseMat function sets an inverse matrix
+        setInverseMat <- function(inverse) inv <<- inverse
+        
+        ## getInverseMat function gets the inverse matrix
+        getInverseMat <- function() inv    
+        
+        ## next, we create a list of the set or get matrix
+        ## in order to can call them as the following example:
+        ## foo <- makeCacheMatrix( "my_matrix")
+        ## another_matrix <- foo$setmat("another matrix")
+        ## the_matrix_is  <- foo$getmat() 
+list(setMat = setMat, getMat=getMat, setInverseMat=setInverseMat, 
+     getInverseMat=getInverseMat)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        ## We retrieve the inverse matrix if exists from the 
+        ## function makeCacheMatrix 
+        inv <- x$getInverseMat()
+        ## We check if indeed the inverse matrix exists
+        if(!is.null(inv)){
+                message("getting cache inverse matrix")
+                return(inv)
+        }
+        ## if there is not an inverse matrix we retreive the matrix
+        ## from makeCacheMatrix function
+        mat_data  <- x$getMat()
+        print(mat_data)
+        inv       <- solve(mat_data)
+        x$setInverseMat(inv)
+        inv
 }
